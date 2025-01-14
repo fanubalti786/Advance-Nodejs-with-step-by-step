dd6916fa0eab356157fe57680a29f401a71f4310
fanubalti786 <fanubalti786@gmail.com>
2025-01-14 16:38:05 +0500

    class04 User and video model with hooks and JWT

---
+++ class04-User_And_VideoModel_With_Hooks_And_JWT/src/db/db.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+dotenv.config(); 
+
+const app = express();
+
+( async ()=>
+{
+    try {
+
+       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}`);
+       console.log(connectionInstance.connection.host);
+
+
+       app.on("error", (error)=>{
+        console.log(`Express connection is failing to establish. ${error}`)
+       });
+
+       app.listen(process.env.PORT,()=>
+        {
+            console.log(`port listning on ${process.env.PORT}`)
+            console.log(`DB Connected`)
+        });
+
+
+    } catch (error) {
+        console.log(`MongoDb Connection Failed ${error}`);
+        process.exit(1);
+        
+    }
+})()
