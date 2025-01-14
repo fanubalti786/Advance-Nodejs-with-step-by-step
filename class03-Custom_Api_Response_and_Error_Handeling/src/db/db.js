4e052e52ecef05bd862ac1067f39616365eeb956
fanubalti786 <fanubalti786@gmail.com>
2025-01-14 12:31:56 +0500

    class03-Custom_api_Response_And_Error_Handeling

---
+++ class03-Custom_Api_Response_and_Error_Handeling/src/db/db.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv'
+import express from 'express';
+import mongoose from 'mongoose';
+const app = express();
+dotenv.config();
+
+( async ()=>
+{
+    try {
+
+       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}`);
+       console.log(connectionInstance.connection.host);
+
+
+       app.on("error", (error)=>{
+        console.log(`Express connection is failing to establish. ${error}`)
+       });
+
+       app.listen(process.env.PORT,()=>
+        {
+            console.log(`port listning on ${process.env.PORT}`)
+            console.log(`DB Connected`)
+        });
+
+
+    } catch (error) {
+        console.log(`MongoDb Connection Failed ${error}`);
+        process.exit(1);
+        
+    }
+})()
---
+++ class03-Custom_Api_Response_and_Error_Handeling/src/index.js
@@ -0,0 +1,11 @@
+
+import dotenv from 'dotenv'
+import express from 'express';
+import '../src/db/db.js';
+const app = express();
+dotenv.config(); 
+
+
+
+
+
