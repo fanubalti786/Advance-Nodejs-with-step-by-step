24f80c54a87838e072e62a1d518ef74382db63eb
fanubalti786 <fanubalti786@gmail.com>
2025-01-23 01:33:16 +0500

    update

--- class05-How_To_Upload_File_In_Backend/src/utlis/Cloudinary.js
+++ class05-How_To_Upload_File_In_Backend/src/utlis/Cloudinary.js
@@ -3,8 +3,8 @@ import fs from 'fs'
 
     // Configuration
     cloudinary.config({ 
-        cloud_name: CLOUDINARY_CLOUD_NAME, 
-        api_key: CLOUDINARY_API_KEY, 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
         api_secret: process.env.LOUDINARY_API_SECRET
     });
 
