196c5aaa5906595d2ba2e8f78edec8c3c5d79b8f
fanubalti786 <fanubalti786@gmail.com>
2025-01-23 15:46:18 +0500

    class06 Router and Controller with debuging

---
+++ class06-Router_And_Controller_with_Debuging/src/db/db.js
@@ -0,0 +1,38 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
+dotenv.config(); 
+
+const app = express();
+
+( async ()=>
+{ 
+    try {
+
+       const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}`);
+       console.log(connectionInstance.connection.host);
+
+
+       app.on("error", (error)=>{
+        console.log(`Express connection is failing to establish. ${error}`)
+       });
+
+
+
+    } catch (error) {
+        console.log(`MongoDb Connection Failed ${error.message}`);
+        process.exit(1);
+        
+    }
+})()
+.then(()=>
+{
+    app.listen(process.env.PORT,()=>
+        {
+            console.log(`port listning on ${process.env.PORT}`)
+            console.log(`DB Connected`)
+        });
+})
+.catch((error)=>{
+    console.log(`mongo db connection is failed : ${error}`)
+});
