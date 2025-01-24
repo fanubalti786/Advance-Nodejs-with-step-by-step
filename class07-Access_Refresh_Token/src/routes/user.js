cbaf85caecfdfac44723cd894cb30b692b919fa5
fanubalti786 <fanubalti786@gmail.com>
2025-01-24 18:16:29 +0500

    auth.js meddleware file done for logout user

--- class07-Access_Refresh_Token/src/routes/user.js
+++ class07-Access_Refresh_Token/src/routes/user.js
@@ -1,6 +1,7 @@
 import Router from "express";
-import {loginUser, registerUser} from "../controllers/user.js";
+import {loginUser, registerUser, logoutUser} from "../controllers/user.js";
 import upload from "../middlewares/multer.js";
+import verifyJWT from "../middlewares/auth.js";
 const router = Router();
 
 
@@ -18,7 +19,10 @@ router.route("/register").post(
     registerUser);
 
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
+
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser);
 
 
 
