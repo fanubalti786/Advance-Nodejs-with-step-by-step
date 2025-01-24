5963c201902c2a5057425c6cfe742d1b2f4c1257
fanubalti786 <fanubalti786@gmail.com>
2025-01-24 23:07:25 +0500

    new route and new controller for access new refreshAccessToken

--- class07-Access_Refresh_Token/src/routes/user.js
+++ class07-Access_Refresh_Token/src/routes/user.js
@@ -1,5 +1,5 @@
 import Router from "express";
-import {loginUser, registerUser, logoutUser} from "../controllers/user.js";
+import {loginUser, registerUser, logoutUser, refreshAccessToken} from "../controllers/user.js";
 import upload from "../middlewares/multer.js";
 import verifyJWT from "../middlewares/auth.js";
 const router = Router();
@@ -23,6 +23,8 @@ router.route("/login").post(loginUser);
 
 //secured routes
 router.route("/logout").post(verifyJWT,logoutUser);
+router.route("/refresh-token").post(refreshAccessToken)
+
 
 
 
