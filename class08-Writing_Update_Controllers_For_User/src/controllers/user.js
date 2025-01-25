bcfdb4be07e5c41a1cb0b9a5f4f1e71cf9ce05c3
fanubalti786 <fanubalti786@gmail.com>
2025-01-26 00:06:56 +0500

    Add getCurrentUser Controller

--- class08-Writing_Update_Controllers_For_User/src/controllers/user.js
+++ class08-Writing_Update_Controllers_For_User/src/controllers/user.js
@@ -257,5 +257,11 @@ const changeCurrentPassword = asyncHandler(async (req,res) =>
 
 })
 
+const getCurrentUser = asyncHandler(async (req,res) =>
+{
+    return res.status(200)
+    .json(200, req.user, "current user fetched successfully");
+})
 
-export {registerUser, loginUser, logoutUser, refreshAccessToken};
+
+export {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser};
