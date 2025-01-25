1762eb7ac7c2e71195d627223cfc138e1bc43521
fanubalti786 <fanubalti786@gmail.com>
2025-01-26 00:38:33 +0500

    Add updateUserAvatar controller

--- class08-Writing_Update_Controllers_For_User/src/controllers/user.js
+++ class08-Writing_Update_Controllers_For_User/src/controllers/user.js
@@ -287,6 +287,30 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, "Account details updated successfully"));
 });
 
+const updateUserAvatar = asyncHandler(async (req,res) =>
+{
+    const avatarLocalPath = req.file?.path;
+    if(!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is missing");
+    }
+    
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    if(!avatar.url){
+        throw new ApiError(400, "Error while uploading on avatar");
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                avatar: avatar.url
+            }
+        },
+        {new: true}
+    ).select("-password");
+})
+
+
 
 export {
   registerUser,
